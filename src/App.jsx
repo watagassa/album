f0b566d7b47d4b7c704b5c34742c842360f8cd39
sakuya11 <k23054kk@aitech.ac.jp>
2023-09-03 02:58:48 +0900

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Sidebar from "./components/Sidebar";
+import ImageUploader from "./ImageUploader";
+function App() {
+  return (
+    <div className="App">
+      {/* uplodaer */}
+      <ImageUploader />
+      <Sidebar />
+    </div>
+  );
+}
+export default App;
